ff6f58d143e29aa4a367f3ed713e56861045106e
yashwant gosavi <yashwantgosavi0@gmail.com>
2023-10-20 17:24:48 +0530

    update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+// text fading
+// const $paragraph = $("p, a, button, h1, h2, h3, h4,h5,h6, img,span");
+// $(window).scroll(function() {
+//   $paragraph.each(function() {
+//     const paragraphMiddle = $(this).offset().top + 0.5 * $(this).height();
+//     const windowBottom = $(window).scrollTop() + $(window).height();
+//     if (paragraphMiddle < windowBottom) {
+//       $(this).addClass("slideIn");
+//     }
+//   });
+// });
+// $(window).scroll();
+
+
+
 let cards = document.querySelectorAll('#accordionExample .card');
 
 cards.forEach(card => {
